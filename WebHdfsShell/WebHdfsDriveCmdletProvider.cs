5c9a1f45a947b880315166fbd9422d228184420c
Chris Duck <gpduck@whatsupduck.net>
2013-09-23 23:49:27 -0500

    initial commit

---
+++ WebHdfsShell/WebHdfsDriveCmdletProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Provider;
+
+namespace Net.WhatsUpDuck.WebHdfsShell {
+	[CmdletProvider("WebHdfs", ProviderCapabilities.Credentials)]
+	public class WebHdfsDriveCmdletProvider : NavigationCmdletProvider {
+		protected override PSDriveInfo NewDrive(PSDriveInfo drive) {
+
+			//Check the drive parameter
+			if(drive == null) {
+				ErrorRecord er = new ErrorRecord(
+						new ArgumentNullException("drive"),
+						"NullDrive",
+						ErrorCategory.InvalidArgument,
+						null);
+				WriteError(er);
+				return null;
+			}
+
+			//Check the Root isn't null
+			if (String.IsNullOrEmpty(drive.Root)) {
+				ErrorRecord er = new ErrorRecord(
+						new ArgumentException("drive.Root"),
+						"NoRoot",
+						ErrorCategory.InvalidArgument,
+						drive);
+				WriteError(er);
+				return null;
+			}
+
+
+			//Check the Root is valid
+			Uri hdfsRootUri = new Uri(drive.Root);
+			if (hdfsRootUri.Scheme != "webhdfs") {
+				ErrorRecord er = new ErrorRecord(
+						new ArgumentException("drive.Root"),
+						"InvalidRoot",
+						ErrorCategory.InvalidArgument,
+						drive);
+				WriteError(er);
+				return null;
+			}
+
+			//Save the drive ino
+			WebHdfsDriveInfo hdfsDriveInfo = new WebHdfsDriveInfo(drive);
+			hdfsDriveInfo.Client = new Microsoft.Hadoop.WebHDFS.WebHDFSClient(hdfsRootUri, drive.Credential.UserName);
+
+			return hdfsDriveInfo;
+		}
+
+		protected override PSDriveInfo RemoveDrive(PSDriveInfo drive) {
+			return base.RemoveDrive(drive);
+		}
+
+		protected override void GetItem(string path) {
+			throw new NotImplementedException();
+		}
+
+		protected override bool ItemExists(string path) {
+			WebHdfsDriveInfo driveInfo = this.PSDriveInfo as WebHdfsDriveInfo;
+			try {
+				driveInfo.Client.GetFileStatus(path);
+				return true;
+			} catch {
+				return false;
+			}
+		}
+
+		protected override bool IsValidPath(string path) {
+			WebHdfsDriveInfo driveInfo = this.PSDriveInfo as WebHdfsDriveInfo;
+			return true;
+		}
+	}
+}
---
+++ WebHdfsShell/WebHdfsDriveInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Provider;
+using System.Net;
+using Microsoft.Hadoop.WebHDFS;
+
+namespace Net.WhatsUpDuck.WebHdfsShell {
+	internal class WebHdfsDriveInfo : PSDriveInfo {
+		public WebHDFSClient Client { get; set; }
+
+		public WebHdfsDriveInfo(PSDriveInfo driveInfo) : base(driveInfo) { }
+	}
+}
